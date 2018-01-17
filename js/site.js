fa2a9fbef40502b29fb716622b5b2f76c0765967
Nicholas Montanino <montanino@utexas.edu>
2018-01-16 21:57:37 -0500

    Missed a comma

--- js/site.js
+++ js/site.js
@@ -7,5 +7,5 @@ $('.header-content').hide().fadeIn(1600);
 var scroll = new SmoothScroll('a[href*="#"]', {
  	speed: 800, // Integer. How fast to complete the scroll in milliseconds
  	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
- 	easing: 'easeInOutCubic', // Easing pattern to use
+ 	easing: 'easeInOutCubic' // Easing pattern to use
 });
