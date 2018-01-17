1770279b23a3d0f381b1ebb3d979d5c6901ad5e9
Nicholas Montanino <montanino@utexas.edu>
2018-01-16 20:36:09 -0500

    Modify smooth scrolling

--- js/site.js
+++ js/site.js
@@ -1,6 +1,12 @@
 'use strict';
 
 // Creates new smooth scroll object
-var scroll = new SmoothScroll('a[href*="#"]');
+var scroll = new SmoothScroll('a[href*="#"]', {
+	// Speed & Easing
+ 	speed: 800, // Integer. How fast to complete the scroll in milliseconds
+ 	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+ 	easing: 'easeInOutCubic', // Easing pattern to use
+});
 
-// $('.main-img').hide().fadeIn(600);
+// Fade in header contents
+$('.header-content').hide().fadeIn(1600);
