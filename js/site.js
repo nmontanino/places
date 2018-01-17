742396771934396d007095a5aae41c83c31b8bda
Nicholas Montanino <montanino@utexas.edu>
2018-01-16 21:34:47 -0500

    Trimmed project files not relating to portfolio

--- js/site.js
+++ js/site.js
@@ -1,12 +1,11 @@
 'use strict';
 
+// Fade in header contents
+$('.header-content').hide().fadeIn(1600);
+
 // Creates new smooth scroll object
 var scroll = new SmoothScroll('a[href*="#"]', {
-	// Speed & Easing
  	speed: 800, // Integer. How fast to complete the scroll in milliseconds
  	offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
  	easing: 'easeInOutCubic', // Easing pattern to use
 });
-
-// Fade in header contents
-$('.header-content').hide().fadeIn(1600);
